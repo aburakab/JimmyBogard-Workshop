3e6c7938bc33b8f1b35e94aad2b37bb789b7657d
Mohammad Aljaber <aburakab@gmail.com>
2019-05-20 12:23:43 +0100

    Init

---
+++ Divergent.Sales.Messages/Events/OrderSubmittedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divergent.Sales.Messages.Events
+{
+    public class OrderSubmittedEvent
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+
+        public List<int> Products { get; set; }
+    }
+}
